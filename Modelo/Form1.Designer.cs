095d94d7316298b97cf423eb686a722e640163a9
Matheus Franco <matheusefranco@gmail.com>
2021-04-19 08:58:42 -0300

    pilhaC#

---
+++ Modelo/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Modelo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSobre = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.listHistorico = new System.Windows.Forms.ListBox();
+            this.btnIr = new System.Windows.Forms.Button();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.webNavegador = new System.Windows.Forms.WebBrowser();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(63)))), ((int)(((byte)(159)))));
+            this.panel1.Controls.Add(this.btnSobre);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(119, 773);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnSobre
+            // 
+            this.btnSobre.FlatAppearance.BorderSize = 0;
+            this.btnSobre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSobre.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSobre.ForeColor = System.Drawing.Color.White;
+            this.btnSobre.Location = new System.Drawing.Point(0, 0);
+            this.btnSobre.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSobre.Name = "btnSobre";
+            this.btnSobre.Size = new System.Drawing.Size(119, 72);
+            this.btnSobre.TabIndex = 9;
+            this.btnSobre.Text = "Sobre";
+            this.btnSobre.UseVisualStyleBackColor = true;
+            this.btnSobre.Click += new System.EventHandler(this.BtnSobre_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(0)))), ((int)(((byte)(238)))));
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.btnFechar);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(119, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1045, 123);
+            this.panel2.TabIndex = 2;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(235, 173);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(668, 34);
+            this.txtUrl.TabIndex = 3;
+            // 
+            // listHistorico
+            // 
+            this.listHistorico.FormattingEnabled = true;
+            this.listHistorico.ItemHeight = 28;
+            this.listHistorico.Location = new System.Drawing.Point(995, 173);
+            this.listHistorico.Name = "listHistorico";
+            this.listHistorico.Size = new System.Drawing.Size(157, 200);
+            this.listHistorico.TabIndex = 4;
+            // 
+            // btnIr
+            // 
+            this.btnIr.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIr.Image = global::Modelo.Properties.Resources.go;
+            this.btnIr.Location = new System.Drawing.Point(909, 173);
+            this.btnIr.Name = "btnIr";
+            this.btnIr.Size = new System.Drawing.Size(61, 34);
+            this.btnIr.TabIndex = 6;
+            this.btnIr.UseVisualStyleBackColor = true;
+            this.btnIr.Click += new System.EventHandler(this.btnIr_Click);
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVoltar.Image = global::Modelo.Properties.Resources.back;
+            this.btnVoltar.Location = new System.Drawing.Point(164, 173);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(61, 34);
+            this.btnVoltar.TabIndex = 5;
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Modelo.Properties.Resources.sinfo;
+            this.pictureBox1.Location = new System.Drawing.Point(193, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(456, 145);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.FlatAppearance.BorderSize = 0;
+            this.btnFechar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFechar.Image = global::Modelo.Properties.Resources.close;
+            this.btnFechar.Location = new System.Drawing.Point(913, 0);
+            this.btnFechar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(132, 87);
+            this.btnFechar.TabIndex = 12;
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.BtnFechar_Click);
+            // 
+            // webNavegador
+            // 
+            this.webNavegador.Location = new System.Drawing.Point(164, 228);
+            this.webNavegador.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webNavegador.Name = "webNavegador";
+            this.webNavegador.Size = new System.Drawing.Size(806, 495);
+            this.webNavegador.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1164, 773);
+            this.Controls.Add(this.webNavegador);
+            this.Controls.Add(this.btnIr);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.listHistorico);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSobre;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.ListBox listHistorico;
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Button btnIr;
+        private System.Windows.Forms.WebBrowser webNavegador;
+    }
+}
+
---
+++ Modelo/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modelo
+{
+    public partial class Form1 : Form
+    {
+        Stack<String> pilha = new Stack<String>(); //global
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnSobre_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void mostraPilha()
+        {
+            listHistorico.Items.Clear();
+            foreach(String url in pilha)
+            {
+                listHistorico.Items.Add(url+"\r\n");
+            }
+        }
+        //-------------------------------
+        private void btnIr_Click(object sender, EventArgs e)
+        {
+            pilha.Push(txtUrl.Text);
+            webNavegador.Navigate(txtUrl.Text);
+            mostraPilha();
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            if (pilha.Count > 1)
+            {
+                pilha.Pop(); // tirando o endereço atual do topo
+                txtUrl.Text = pilha.Pop();
+                webNavegador.Navigate(txtUrl.Text);
+                mostraPilha();
+            }// fim if
+            else
+                MessageBox.Show("Não tenho para onde voltar :(");
+        }// fim metodo voltar
+    }
+}
